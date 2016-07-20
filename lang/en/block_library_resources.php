c90c1f2db2eb9cc5f01ffc89809235f77284d436
roglos <roelmann@glos.ac.uk>
2016-07-20 10:52:25 +0100

    first commit

---
+++ lang/en/block_library_resources.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+ * Name: Library Resources Block
+ * Version: 1.2.4
+ * Author: Paul Griffiths
+ * Created: 1st November 2012
+ * Modified: 24th January 2013
+ *
+ */
+
+$string['pluginname'] = 'Library Resources block';
+$string['library_resources'] = 'Library Resources';
+$string['library_resources:addinstance'] = 'Add a new Library Resources block';
+$string['library_resources:myaddinstance'] = 'Add a new Library Resources block to the My Moodle page';
---
+++ version.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * Name: Library Resources Block
+ * Version: 1.2.4
+ * Author: Paul Griffiths
+ * Created: 1st November 2012
+ * Modified: 3rd July 2014
+ *
+ */
+
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version = 2014070300;  // YYYYMMDDHH (year, month, day, 24-hr time)
+$plugin->requires = 2011120504; // YYYYMMDDHH (This is the release version for Moodle 2.2.4)
+$plugin->component = 'block_library_resources'; // Full name of the plugin (used for diagnostics)
